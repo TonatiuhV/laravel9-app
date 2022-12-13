8301baf6205aee8ab5ba2b493c5b1f6f97b25e65
tonatiuh <tonatiuhvalladares@gmail.com>
2022-12-12 19:41:59 -0600

    plantilla blade herencia

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Blog</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-
-    </head>
-    <body>
-        @include('partials.navigation')
-        <h1>Blog</h1>
-    </body>
-</html>
+@extends('layouts.app')
+@section('content')
+<h1>Blog</h1>
+@endsection
