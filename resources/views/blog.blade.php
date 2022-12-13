67d4bc6e471edcf11ae58cc709aa8edf4376fb54
tonatiuh <tonatiuhvalladares@gmail.com>
2022-12-12 19:35:55 -0600

    navegacion blade herencia

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -12,6 +12,7 @@
 
     </head>
     <body>
+        @include('partials.navigation')
         <h1>Blog</h1>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,12 +21,7 @@
         </style>
     </head>
     <body >
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="{{route('blog')}}">Blog</a></li>
-            <li><a href="{{route('about')}}">About</a></li>
-            <li><a href="{{route('contact')}}">Contacto</a></li>
-        </ul>
+      @include('partials.navigation')
         <h1>Inicio</h1>
     </body>
 </html>
